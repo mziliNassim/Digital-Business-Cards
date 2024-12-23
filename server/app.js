f04c2e67315e80bd5ff7b8767d800a342e4563ba
mziliNassim <mzilinassim@gmail.com>
2024-12-23 23:41:24 +0100

    Server .env handling...!

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./db/connectDB");
 const { config } = require("dotenv");
 
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -12,15 +11,15 @@ config();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.get("/", (req, res) => {
-  const host_URL = "http://localhost:3001/api/auth";
   res.json({
-    "check-auth": host_URL + "/check-auth",
-    register: host_URL + "/register",
-    login: host_URL + "/login",
-    logout: host_URL + "/logout",
-    "verify-email": host_URL + "/verify-email",
-    "forgot-password": host_URL + "/forgot-password",
-    "reset-password/:token": host_URL + "/reset-password/:token",
+    "check-auth": process.env.SERVER_URL + "/api/auth/check-auth",
+    register: process.env.SERVER_URL + "/api/auth/register",
+    login: process.env.SERVER_URL + "/api/auth/login",
+    logout: process.env.SERVER_URL + "/api/auth/logout",
+    "verify-email": process.env.SERVER_URL + "/api/auth/verify-email",
+    "forgot-password": process.env.SERVER_URL + "/api/auth/forgot-password",
+    "reset-password/:token":
+      process.env.SERVER_URL + "/api/auth/reset-password/:token",
   });
 });
 
